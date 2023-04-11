ad167d36a1379df3f9fd8c6cfb5ea4c3738bafc7
drew-chidi <chidi.andrew@gmail.com>
2023-04-11 11:15:20 +0100

    refactor the about me section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,6 +10,7 @@ import {
   services,
 } from '../../data/index';
 import classes from './About.module.css';
+import SectionLayout from '@/components/Layout/SectionLayout';
 
 const About = ({ setSelectedPage }) => {
   const theme = useSelector((state) => state.theme.mode);
@@ -19,79 +20,79 @@ const About = ({ setSelectedPage }) => {
 
   return (
     <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.About)}>
-      <section
-        id='about'
-        className={`${classes.s_about} ${classes.target_section} ${
-          theme === 'dark' && classes.themeMode
-        }`}
-      >
-        <div className={classes.container}>
-          {/* Title */}
-          <div>
-            <h2 className={classes.section_heading}>About me</h2>
-            <hr className={classes.divider} />
-          </div>
-          {/* a bit about myself */}
-          <div className={classes.row}>
-            <h3>{dataabout.title}</h3>
-            <p className={classes.aboutme_p}>{dataabout.aboutme}</p>
-          </div>
+      <SectionLayout id='about' title='About me'>
+        <section
+          id='about'
+          className={`${classes.s_about} ${classes.target_section} ${
+            theme === 'dark' && classes.themeMode
+          }`}
+        >
+          <div className={classes.container}>
+            {/* a bit about myself */}
+            <div className={classes.row}>
+              <h3>{dataabout.title}</h3>
+              <p className={classes.aboutme_p}>{dataabout.aboutme}</p>
+            </div>
 
-          {/* Work Timeline */}
-          <div className={classes.row}>
-            <h3>Work Timeline</h3>
-            <div className={classes.col}>
-              <table className={`${classes.table} ${classes.caption_top}`}>
-                <tbody>
-                  {worktimeline.map((data, i) => {
-                    return (
-                      <tr key={i} className={classes.table_row}>
-                        <th scope='row' className={classes.cell_wrapper}>
-                          {data.jobtitle}
-                        </th>
-                        <td className={classes.cell_wrapper}> {data.where}</td>
-                        <td className={classes.cell_wrapper}>{data.date}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+            {/* Work Timeline */}
+            <div className={classes.row}>
+              <h3>Work Timeline</h3>
+              <div className={classes.col}>
+                <table className={`${classes.table} ${classes.caption_top}`}>
+                  <tbody>
+                    {worktimeline.map((data, i) => {
+                      return (
+                        <tr key={i} className={classes.table_row}>
+                          <th scope='row' className={classes.cell_wrapper}>
+                            {data.jobtitle}
+                          </th>
+                          <td className={classes.cell_wrapper}>
+                            {' '}
+                            {data.where}
+                          </td>
+                          <td className={classes.cell_wrapper}>{data.date}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            {/* Skills */}
+            <div className={classes.row}>
+              <h3>Skills</h3>
+              <div className={`${classes.skills_wrapper}`}>
+                {' '}
+                {skills.map((data, i) => {
+                  return (
+                    <div key={i}>
+                      <p className={`${classes.skill} ${invertTheme}`}>
+                        {data.name}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+
+            {/* Services */}
+            <div className={classes.row} id='services'>
+              <h3>Services</h3>
+              <div className={classes.services_details}>
+                {services.map((data, i) => {
+                  return (
+                    <div key={i} className={classes.service_wrapper}>
+                      <h5 className={classes.service_title}>{data.title}</h5>
+                      <p className={classes.service_desc}>{data.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
-
-          {/* Skills */}
-          <div className={classes.row}>
-            <h3>Skills</h3>
-            <div className={`${classes.skills_wrapper}`}>
-              {' '}
-              {skills.map((data, i) => {
-                return (
-                  <div key={i}>
-                    <p className={`${classes.skill} ${invertTheme}`}>
-                      {data.name}
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-
-          {/* Services */}
-          <div className={classes.row} id='services'>
-            <h3>Services</h3>
-            <div className={classes.services_details}>
-              {services.map((data, i) => {
-                return (
-                  <div key={i} className={classes.service_wrapper}>
-                    <h5 className={classes.service_title}>{data.title}</h5>
-                    <p className={classes.service_desc}>{data.description}</p>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </SectionLayout>
     </motion.div>
   );
 };
